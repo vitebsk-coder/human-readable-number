733d7113bb11b8aae323d4a88179e82f9c1f5698
Egor <egor.marmilev96@gmail.com>
2024-08-04 21:22:20 +0300

    fix: correct code human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 module.exports = function toReadable(number) {
-   const arr1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
-   const arr2 = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-   const arr3 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-
+   const arr1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'],
+      arr2 = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+      arr3 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
    let res = '';
 
    if (number >= 0 && number <= 10) {
-      res += arr1[n];
+      res += arr1[number];
    } else if (number > 10 && number <= 19) {
-      res += arr2[n.toString().slice(1) - 1];
+      res += arr2[number.toString().slice(1) - 1];
    } else if (number >= 20 && number <= 99) {
       const tens = Math.floor(number / 10);
       const ones = number % 10;
-      res += arr3[tens - 2];
+      res += arr3[tens - 2]
       if (ones > 0) {
-         res += ` ${arr1[ones]}`;
+         res += ` ${arr1[ones]}`
       }
    } else if (number >= 100 && number <= 999) {
       const hundreds = Math.floor(number / 100);
       const remainder = number % 100;
       res += `${arr1[hundreds]} hundred`;
       if (remainder > 0) {
-         res += ` ${numberToText(remainder)}`;
+         res += ` ${toReadable(remainder)}`
       }
-      return res;
    }
+   return res;
 }
+
 
